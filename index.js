28bd966ab03c94aabb96509f6ef20f6a62050681
ZippMukami <zimukami@gmail.com>
2022-02-06 18:21:10 +0300

    This commit adds javascript code that helps in selecting answers and giving a score

---
+++ index.js
@@ -0,0 +1,16 @@
+ function check() {
+     var c = 0;
+     var q1 = document.quiz.question1.value;
+     var q2 = document.quiz.question2.value;
+     var q3 = document.quiz.question3.value;
+     var q4 = document.quiz.question4.value;
+     var q5 = document.quiz.question5.value;
+     var q6 = document.quiz.question6.value;
+     if (q1= "a") {c++}
+     if (q2= "c") {c++}
+     if (q3= "d") {c++}
+     if (q4= "d") {c++}
+     if (q5= "c") {c++}
+     if (q6= "a") {c++}
+     document.write(c);
+ }
