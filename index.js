31844b7574fb275da7b4da2fe0eae5b3ac72abed
ZippMukami <zimukami@gmail.com>
2022-02-06 18:50:58 +0300

    This commit adds the page background color. Style.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
  function check() {
      var c = 0;
-     var q1 = document.quiz.question1.value;
-     var q2 = document.quiz.question2.value;
-     var q3 = document.quiz.question3.value;
-     var q4 = document.quiz.question4.value;
-     var q5 = document.quiz.question5.value;
-     var q6 = document.quiz.question6.value;
-     if (q1= "a") {c++}
-     if (q2= "c") {c++}
-     if (q3= "d") {c++}
-     if (q4= "d") {c++}
-     if (q5= "c") {c++}
-     if (q6= "a") {c++}
+     var q1 = document.quizHTML.question1.value;
+     var q2 = document.quizHTML.question2.value;
+     var q3 = document.quizHTML.question3.value;
+     var q4 = document.quizHTML.question4.value;
+     var q5 = document.quizHTML.question5.value;
+     var q6 = document.quizHTML.question6.value;
+     if (q1== "Hyper Text Markup Language") {c++}
+     if (q2== "class") {c++}
+     if (q3== "html") {c++}
+     if (q4== "Styling html elements") {c++}
+     if (q5== "It informs the browser about what document type to expect") {c++}
+     if (q6== "Help in documenting your source code") {c++}
+
+     
      document.write(c);
  }
